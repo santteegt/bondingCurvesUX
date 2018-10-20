4401bfb385161c358664ed5dc459e46f0006aa2e
jonassnellinckx <jo.snel@hotmail.com>
2018-10-20 23:22:20 +0200

    fix gh-pages

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
+import { Router } from 'react-router';
 
 import './assets/stylesheets/global.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import createHashHistory from 'history/createHashHistory';
+
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <App />
+    </Router>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
